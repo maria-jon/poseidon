2ed68fe4f36e434fc5d5c323dcff9ec5bc85a77e
maria-jon <mariajonasson93@gmail.com>
2025-03-05 16:23:27 +0100

    (feat): add HeaderBar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import HeaderBar from './components/HeaderBar.vue';
 </script>
 
 <template>
   <header>
+    <HeaderBar />
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <img src="@/assets/Logotype_Poseidons-retreat.svg">
+    <button class="hamburger">
+      <img src="@/assets/Icon-burger.svg">
+    </button>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.wrapper {
+  background-color: $color-primary-dark;
+  color: $color-white;
+  border: none;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  position: sticky;
+  top: 0;
+  padding-right: $margin-medium;
+}
+
+.hamburger {
+  height: 42px;
+  width: 54px;
+  border: 1px solid $color-error-red;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+}
+
+button {
+  background-color: transparent;
+}
+
+</style>
