60b4517cecc0f2c9f7824819d5f4ed2c968d1ec0
JanisSeibutis <janis.seibutis@gmail.com>
2025-03-05 12:54:00 +0100

    (feat): create basic main button

---
+++ src/components/MainButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>Button text</button>
+</template>
+
+<style scoped>
+button {
+  background-color: #c45c54;
+  border-radius: 8px;
+  color: #fcf9f7;
+  border: none;
+  height: 38px;
+  width: fit-content;
+}
+</style>
