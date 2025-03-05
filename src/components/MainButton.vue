0b9035254e8d530cc634eac5dd730148813c6522
JanisSeibutis <janis.seibutis@gmail.com>
2025-03-05 17:45:48 +0100

    (style): add box shadow to button

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -19,10 +19,12 @@ button {
   height: 38px;
   width: fit-content;
   transition: all 0.2s ease-in-out;
+  box-shadow: 0px 1px 1px 0px rgba(0, 0, 0, 0.25); // x: 0, y: 1, blur: 1, spread: 0
 
   &:hover {
     background-color: $color-secondary-red-dark;
     font-size: 1.125rem;
+    box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.25);
   }
 }
 </style>
