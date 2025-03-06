5169d459541c1412c156b9176500228a3b5e9d70
SarahSu92 <sarah.sundberg@outlook.com>
2025-03-06 14:44:55 +0100

    (feat): Mobileversion for AboutUs page is done.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-
+import AboutUs from '@/components/AboutUs.vue';
 </script>
 
 <template>
-  
+  <main>
+    <AboutUs />
+  </main>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
