c671f6e32e5e2cd1a1387328fa8cda5c787dc2f1
Yuliia-fed23 <147343499+Yuliia-fed23@users.noreply.github.com>
2025-03-06 20:22:15 +0100

    (style): add styles for menu footer, create new component

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
 import { ref } from "vue";
-
-const currentYear = ref(new Date().getFullYear());
+import FooterIcons from "./footerIcons/FooterIcons.vue";
 </script>
 
 <template>
     <footer class="menu-footer">
       <div class="menu-container">
-        <div class="menu-icons">
-          <a href="" target="_blank">
-            <i class="instagram">Instagram</i>
-          </a>
-          <a href="" target="_blank">
-            <i class="instagram">Instagram</i>
-          </a>
-          <a href="" target="_blank">
-            <i class="instagram">Instagram</i>
-          </a>
-        </div>
+
+         <FooterIcons />
+
         <nav class="menu-nav">
           <ul class="menu-list">
             <li><a href="#home">Home</a></li>
@@ -26,7 +17,7 @@ const currentYear = ref(new Date().getFullYear());
             <li><a href="#book">Book Now</a></li>
           </ul>
         </nav>
-        <p class="privacy-text">Copyright &copy;  {{ new Date().getFullYear() }} All rights reserved.</p>
+        <p class="privacy-text">Privacy policy |  Icons made by <a href="https://www.flaticon.com/" target="_blank">Freepik</a></p>
       </div>
     </footer>
   </template>
@@ -38,37 +29,51 @@ const currentYear = ref(new Date().getFullYear());
 footer {
   background-color: $color-primary-medium;
   color: #fff;
-  min-height: 200px;
+  height: 200px;
   position: relative;
- 
-
-  .menu-icons {
-    display: flex;
-    justify-content: center;
-    gap: 1rem;
-    padding: 1rem 0;
-  }
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  background-origin: border-box;
+  margin-top: 3rem;
   ul {
     list-style: none;
-    margin: 0;
-    padding: 0;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
     display: flex;
     justify-content: center;
     gap: 1rem;
    }
-  li a {
+    .menu-list a {
     color: #fff;
+    display: inline-block;
+    font-size: 1.3rem;
+    padding-left: 0.6rem;
+    &:hover {
+      color: $color-primary-light;
+      text-decoration: none;
+    }
   }
-}
+
 .privacy-text {
     text-align: center;
     position: absolute;
-    font-size: 0.8rem;
+    font-weight: 400;
+    font-size: 1rem;
     color: #fff;
-    background-color: #334D4D;
+    background-color: $color-primary-dark;
     padding: 0.5rem;
     bottom: 0;
     width: 100%;
+    left: 0;
+
+    a{
+        color: #fff;
+        font-weight: 400;
+        font-size: 1rem;
+        text-decoration: underline;
+    }
+  }
 }
 </style>
