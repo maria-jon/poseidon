2445ba394b6602ac482e1e7f541f0b354e3c2314
maria-jon <mariajonasson93@gmail.com>
2025-03-07 13:45:24 +0100

    (feat): add radio buttons

--- src/components/BookingCard.vue
+++ src/components/BookingCard.vue
@@ -9,13 +9,14 @@ defineProps({
 </script>
 
 <template>
-  <dialog open="true">
-    <h5>{{ header }}</h5>
+  <div class="complete-your-booking">
+    <section>
+      <h5>{{ header }}</h5>
     <form>
       <label>
         <span class="input-label info">Name</span>
         <div class="input-field">
-          <input text="name" placeholder="Full Name">
+          <input class="text-input" text="name" placeholder="Full Name">
           <span class="material-symbols-outlined filled">
             info
           </span>
@@ -24,7 +25,7 @@ defineProps({
       <label>
         <span class="input-label info">Email address</span>
         <div class="input-field">
-          <input text="name" placeholder="example@website.com">
+          <input class="text-input" text="name" placeholder="example@website.com">
           <span class="material-symbols-outlined filled">
             info
           </span>
@@ -33,39 +34,57 @@ defineProps({
       <label>
         <span class="input-label info">Phone number</span>
         <div class="input-field">
-          <input text="name" placeholder="+00 000 00 00">
+          <input class="text-input" text="name" placeholder="+00 000 00 00">
           <span class="material-symbols-outlined filled">
             info
           </span>
         </div>
       </label>
     </form>
+  </section>
+    <section>
+      <h5>Payment options</h5>
+      <div class="radios">
+        <label>
+          <input class="radio-input" type="radio" value="credit">
+          <span class="input-label info">Pay with credit card</span>
+        </label>
+        <label>
+          <input class="radio-input" type="radio" value="paypal">
+          <span class="input-label info">Pay with PayPal</span>
+        </label>
+        <label>
+          <input class="radio-input" type="radio" value="arrival">
+          <span class="input-label info">Pay on arrival</span>
+        </label>
+      </div>
+    </section>
     <MainButton :label="label" :width="width" />
-  </dialog>
+  </div>
 </template>
 
 <!-- Används så att importera först och sedan använd som
 <PopupConfirmation header="rubriken" text="paragraph texten" label="knapptexten" width="önskad knappens bredd"/>  -->
 
 <style scoped lang="scss">
-dialog {
+.complete-your-booking {
   box-sizing: border-box;
   position: fixed;
   background-color: $color-primary-light;
   border-radius: 8px;
-  border: none;
   width: 24rem;
   max-width: 90%;
-  height: 20rem;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  padding-left: 24px;
-  padding-top: 24px;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: $margin-small;
 }
 
 h5 {
-  margin-bottom: 32px;
+  margin-bottom: $margin-small;
 }
 
 form {
@@ -78,10 +97,11 @@ form {
   display: flex;
 }
 
-input {
+.text-input {
   border-radius: 10px;
   border: 2px solid $color-primary-dark;
-  width: 200px;
+  width: 50%;
+  padding: 2px;
 }
 
 .material-symbols-outlined {
@@ -92,6 +112,20 @@ input {
 
 .filled {
   font-variation-settings: 'FILL' 1;
+}
+
+.radios {
+  background-color: $color-background-light;
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  padding: 20px;
+}
+.radio-input {
+  margin: $margin-xsmall;
+  width: 1.5rem;
+  height: 1.5rem;
+  accent-color: $color-error-red;
 }
 
 button {
