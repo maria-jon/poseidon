62a124016949f1c92eaf861a5bfd0fb110bdb58f
JanisSeibutis <janis.seibutis@gmail.com>
2025-03-07 12:14:54 +0100

    (feat): create & display packages card with image

---
+++ src/components/PackagesCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  header: String,
+  subheader: String,
+  subtext: String,
+  features: { type: Array, items: String },
+  image: String,
+  id: Number,
+})
+</script>
+
+<template>
+  <article>
+    <img :src="image" alt="" />
+  </article>
+</template>
+
+<style scoped lang="scss">
+article {
+  width: 100%;
+  height: 44.5rem;
+}
+
+img {
+  width: 20rem;
+}
+</style>
--- src/views/PackagesView.vue
+++ src/views/PackagesView.vue
@@ -1,9 +1,69 @@
 <script setup lang="ts">
 import HeaderBar from '@/components/HeaderBar.vue'
+import PackagesCard from '@/components/PackagesCard.vue'
+import { usePackageStore } from '@/stores/packages'
+import { storeToRefs } from 'pinia'
+
+const packageStore = usePackageStore()
+const { packages } = storeToRefs(packageStore)
 </script>
 
 <template>
   <HeaderBar />
+  <header>
+    <h3>Our packages</h3>
+    <p>
+      Make your stay at Poseidon’s Retreat even more magical with our exclusive package deals.
+      Whether you're looking for a romantic getaway, a deep-sea adventure, or ultimate relaxation,
+      we have the perfect offer for you.
+    </p>
+    <span>Dive in and discover luxury beneath the waves!</span>
+  </header>
+
+  <main>
+    <div class="wrapper" v-for="singlePackage in packages" :key="singlePackage.id">
+      <PackagesCard :image="singlePackage.image" />
+    </div>
+  </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 4rem;
+}
+
+header {
+  // width: 90%;
+  max-width: 22rem;
+  text-align: center;
+  height: 17rem;
+  margin: 2rem auto;
+  text-align: left;
+  // padding: 0 1rem;
+}
+
+h3 {
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+p {
+  margin-bottom: 1rem;
+}
+
+.wrapper {
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  max-width: 22rem;
+  background-color: $color-primary-light;
+  margin-bottom: 2rem;
+}
+</style>
