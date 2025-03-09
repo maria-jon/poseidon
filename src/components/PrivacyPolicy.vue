14ea7b164ecf39fa489f6361ca0f6f5a4e0c803c
Yuliia-fed23 <147343499+Yuliia-fed23@users.noreply.github.com>
2025-03-09 22:50:14 +0100

    (style): add styles  for privacy page

--- src/components/PrivacyPolicy.vue
+++ src/components/PrivacyPolicy.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { useRouter } from "vue-router";
+import poseidonMermaid from '@/assets/Images/Komprimerad poseidon-mermaid_1440x2560_jpg (1).jpg';
+
+// Redirect to the homepage via the router
+const router = useRouter();
 
 // Describe the type for the policy section
 interface PolicySection {
@@ -37,14 +42,14 @@ const policySections = ref<PolicySection[]>([
 
 // Function to go to the main page
 const goHome = () => {
-  console.log("Going back to home");
+  router.push("/");
 };
 </script>
 
 <template>
     <div class="privacy-policy">
       <div class="image-section">
-        <img src="" alt="" />
+        <img :src="poseidonMermaid" class="image-privacy" alt="poseidon mermaid" height="500" weight="400"/>
       </div>
       <div class="content-section">
         <h1>Privacy Policy</h1>
@@ -57,14 +62,104 @@ const goHome = () => {
         <button @click="goHome" class="back-button">BACK TO HOME</button>
       </div>
     </div>
-    <FooterSection />
+   
   </template>
 
 <style scoped lang="scss">
 @import '@/style/variables.scss';
-.content-section h1 {
-  color: black;
+.privacy-policy {
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding-top: 5%;
+  background-color: $color-background-light;
+}
+.image-section {
+  flex: 1;
+  display: flex;
+  justify-content: center; /* Вирівнює картинку зліва */
+  align-items: center;
+  max-width: 40%; /* Обмежує ширину, щоб текст не був надто стислий */
 }
 
+.image-privacy {
+  width: 100%;
+  height: auto;
+  max-width: 400px;
+  border-radius: 5px;
+}
 
+.image-section {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 50%;
+}
+
+.content-section {
+  flex: 1;
+  h1 {
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding-bottom: 30px;
+  }
+
+h2 {
+  font-size: 1.1rem;
+  font-weight: bold;
+  padding-bottom: 15px;
+ }
+}
+
+.policy-section {
+    margin-bottom: 10px;
+   ul {
+    list-style: none;
+    }
+}
+
+.policy-section:nth-child(4) ul {
+    list-style: disc;
+  }
+
+.back-button {
+  background-color: #d9534f;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+@media (max-width: 1024px) {
+  .privacy-policy {
+    padding: 20px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    display: flex;
+    height: auto;
+    box-sizing: border-box;
+}
+
+  .image-section {
+    display: none;
+  }
+
+  .content-section {
+    max-width: 600px;
+    width: 100%;
+    margin-top: 20px;
+    height: auto;
+  }
+  .policy-section {
+    margin-bottom: 15px;
+  }
+  .back-button {
+    margin-top: 30px;
+  }
+}
 </style>
